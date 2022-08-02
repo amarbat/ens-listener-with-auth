0f36e5e5df532739aea4046270d470ae4488b887
AB <amarbat@gmail.com>
2022-08-02 13:25:07 -0400

    change token

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.post("/oauth/token", function (req, res) {
   console.log ("==========================END=============================");
 
   res.send (JSON.parse(JSON.stringify({
-    "access_token": "eyJhbGciOiJIUzI1NiIsImtpZCI6IjQiLCJ2ZXIiOiIxIiwidHlwIjoiSldUIn0.eyJhY2Nlc3NfdG9rZW4iOiI3anNlYnA2NmV4cUtQQktoRlJTam9hSDAiLCJjbGllbnRfaWQiOiJkc2FudXpnc3Y2NGpjYzc1Y2FiMWJreGkiLCJlaWQiOjcyNTc3NjQsInN0YWNrX2tleSI6IlM3IiwicGxhdGZvcm1fdmVyc2lvbiI6MiwiY2xpZW50X3R5cGUiOiJTZXJ2ZXJUb1NlcnZlciIsInBpZCI6MTg2fQ.tBNqAzHctfLhcAYc0gXVOazPM3ekNmklDAEbB8cVLsQ.GmZ5IVFmxDEsf6vNcreVFmYrexB0ZpAA8QU-i1n0K8V8e43X_NiesUf7C-X-osoVj8osXiDvPLPWeTBLKDjYOFTmUfYpvhmNjcnIeFqqAk46KPuPyMykjT_aAIKoUokOI9ORF1HU7NCwntFXJsRYK3bhEP88SG-IQcS3T72X5",
+    "access_token": "7jsebp66exqKPBKhFRSjoaH0",
     "token_type":"bearer",
     "expires_in":36000
   })));
